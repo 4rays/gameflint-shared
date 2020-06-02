4515b867f1884db140d2397099aafa8ce97b0c4a
Reda Lemeden <git@redalemeden.com>
2020-06-02 21:34:55 +0200

    Add missing initializer

--- Sources/FiresideShared/GameSearchResult.swift
+++ Sources/FiresideShared/GameSearchResult.swift
@@ -5,4 +5,16 @@ public struct GameSearchResult: Codable, Equatable, Identifiable, Hashable {
   public var name: String
   public var coverURL: String?
   public var description: String?
+
+  public init(
+    id: UUID,
+    name: String,
+    coverURL: String? = nil,
+    description: String? = nil
+  ) {
+    self.id = id
+    self.name = name
+    self.coverURL = coverURL
+    self.description = description
+  }
 }
