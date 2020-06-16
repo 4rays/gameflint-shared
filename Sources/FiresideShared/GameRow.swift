61265297c5f601a2940d657fceb76b391eb81262
Reda Lemeden <git@redalemeden.com>
2020-06-16 11:54:07 +0200

    Add localized names to game row

--- Sources/FiresideShared/GameRow.swift
+++ Sources/FiresideShared/GameRow.swift
@@ -3,21 +3,24 @@ import Foundation
 public struct GameRow: Codable, Equatable, Identifiable, Hashable {
   public var id: UUID
   public var name: String
+  public var localizedNames: [LocalizedName]
   public var coverHash: String?
-  public var description: String?
+  public var platforms: [Platform]
   public var earliestReleaseDate: Date?
 
   public init(
     id: UUID,
     name: String,
+    localizedNames: [LocalizedName] = [],
     coverHash: String? = nil,
-    description: String? = nil,
+    platforms: [Platform] = [],
     earliestReleaseDate: Date? = nil
   ) {
     self.id = id
     self.name = name
+    self.localizedNames = localizedNames
     self.coverHash = coverHash
-    self.description = description
+    self.platforms = platforms
     self.earliestReleaseDate = earliestReleaseDate
   }
 }
