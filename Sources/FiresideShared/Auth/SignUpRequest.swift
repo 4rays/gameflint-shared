4613ef7ccc7c25137ee9eb2fb3e4731f9daeeded
Reda Lemeden <git@redalemeden.com>
2020-08-03 12:56:06 +0200

    Add new SignUpRequest type

---
+++ Sources/FiresideShared/Auth/SignUpRequest.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct SignUpRequest: Codable, Equatable {
+  public var username: String
+  public var email: String
+  public var password: String
+
+  public init(
+    username: String,
+    email: String,
+    password: String
+  ) {
+    self.username = username
+    self.email = email
+    self.password = password
+  }
+}
