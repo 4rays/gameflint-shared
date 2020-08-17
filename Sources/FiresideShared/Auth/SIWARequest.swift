0b9044999460258e3b4b2111a4ecf95f02e8a3b8
Reda Lemeden <git@redalemeden.com>
2020-08-17 14:41:06 +0200

    Add protocol conformances

--- Sources/FiresideShared/Auth/SIWARequest.swift
+++ Sources/FiresideShared/Auth/SIWARequest.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct SIWARequest {
+public struct SIWARequest: Codable, Equatable, Hashable {
   public var firstName: String?
   public var lastName: String?
   public var email: String?
