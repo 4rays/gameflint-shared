dade7772b833c6a18c24109e987415e4be3bfb86
Reda Lemeden <git@redalemeden.com>
2020-10-01 22:27:38 +0200

    Add ResponseError

--- Sources/FiresideShared/Global/ServerResponse.swift
+++ Sources/FiresideShared/Global/ServerResponse.swift
@@ -1,11 +1,41 @@
 import Foundation
 
 public struct ServerResponse: Codable, Equatable {
-  public var error: Bool
-  public var message: String
+  public var error: ResponseError?
+  public var success: Bool
 
-  public init(error: Bool, message: String) {
+  public init(error: ResponseError? = nil) {
     self.error = error
-    self.message = message
+    self.success = error == nil
   }
 }
+
+public struct ResponseError: Codable, Equatable, Hashable {
+  public var code: Code
+  public var message: String
+
+  public init(_ message: String) {
+    self.code = .genericError
+    self.message = message
+  }
+
+  public init(_ code: Code) {
+    self.code = code
+    self.message = code.description
+  }
+}
+
+public extension ResponseError {
+  enum Code: Int, Codable, Equatable, Hashable {
+    case genericError = 900
+
+    public var description: String {
+      switch self {
+      case .genericError:
+        return "Something went wrong and we're not quite sure what or how."
+      }
+    }
+  }
+}
+
+
