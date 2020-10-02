6a2bda1fbbe481c8c0879448c3fda5ed46906a8e
Reda Lemeden <git@redalemeden.com>
2020-10-02 18:28:03 +0200

    Add extra errors

--- Sources/FiresideShared/Global/ServerResponse.swift
+++ Sources/FiresideShared/Global/ServerResponse.swift
@@ -15,24 +15,39 @@ public struct ResponseError: Codable, Equatable, Hashable {
   public var message: String
 
   public init(_ message: String) {
-    self.code = .genericError
+    self.code = .internalServerError
     self.message = message
   }
 
   public init(_ code: Code) {
     self.code = code
-    self.message = code.description
+    self.message = code.name
   }
 }
 
 public extension ResponseError {
   enum Code: Int, Codable, Equatable, Hashable {
-    case genericError = 900
+    case internalServerError = 900
+    case invalidCredentials = 910
+    case invalidSession
+    case takenEmail
+    case unverifiedEmail
+    case takenUsername
 
-    public var description: String {
+    public var name: String {
       switch self {
-      case .genericError:
-        return "Something went wrong and we're not quite sure what or how."
+      case .internalServerError:
+        return "Internal Server Error"
+      case .invalidCredentials:
+        return "Invalid Credentials"
+      case .invalidSession:
+        return "Invalid Session"
+      case .takenEmail:
+        return "Email is Taken"
+      case .unverifiedEmail:
+        return "Email is Unverified"
+      case .takenUsername:
+        return "Username is Taken"
       }
     }
   }
