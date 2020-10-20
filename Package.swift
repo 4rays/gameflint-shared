c5e72c730cd2c84663d8e9bd89884c12e4ae8234
Reda Lemeden <git@redalemeden.com>
2020-10-20 15:25:34 +0200

    Clean up package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "GameFlintShared",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "GameFlintShared",
-            targets: ["GameFlintShared"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "GameFlintShared",
-            dependencies: []),
-        .testTarget(
-            name: "GameFlintSharedTests",
-            dependencies: ["GameFlintShared"]),
-    ]
+  name: "GameFlintShared",
+  products: [
+    .library(
+      name: "GameFlintShared",
+      targets: ["GameFlintShared"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "GameFlintShared",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "GameFlintSharedTests",
+      dependencies: ["GameFlintShared"]
+    ),
+  ]
 )
