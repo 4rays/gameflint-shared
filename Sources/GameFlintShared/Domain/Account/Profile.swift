c8e6965c63d90aba0b583ca49cfa49eb312fc9d9
Reda Lemeden <git@redalemeden.com>
2020-11-29 13:00:14 +0100

    Add profile struct

---
+++ Sources/GameFlintShared/Domain/Account/Profile.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public struct Profile: Codable, Equatable, Identifiable, Hashable  {
+  public init(
+    id: UUID? = nil,
+    username: String,
+    firewood: Int = 0,
+    displayName: String? = nil,
+    avatarURL: String? = nil,
+    createdAt: Date? = nil,
+    updatedAt: Date? = nil
+  ) {
+    self.id = id
+    self.username = username
+    self.firewood = firewood
+    self.displayName = displayName
+    self.avatarURL = avatarURL
+    self.createdAt = createdAt
+    self.updatedAt = updatedAt
+  }
+
+  public var id: UUID?
+  public var username: String
+  public var firewood: Int
+  public var displayName: String?
+  public var avatarURL: String?
+  public var createdAt: Date?
+  public var updatedAt: Date?
+}
