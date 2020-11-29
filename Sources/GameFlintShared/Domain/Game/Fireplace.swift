4674ca6cc3d6c42b2d10e816c85f6453198b363e
Reda Lemeden <git@redalemeden.com>
2020-11-29 13:01:01 +0100

    Add fireplace struct

---
+++ Sources/GameFlintShared/Domain/Game/Fireplace.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public struct Fireplace: Codable, Equatable, Identifiable, Hashable {
+  public var id: UUID?
+  public var firewood: Int
+  public var createdAt: Date?
+  public var updatedAt: Date?
+  public var reviews: [Review]
+
+  public init(
+    id: UUID? = nil,
+    firewood: Int,
+    createdAt: Date? = nil,
+    updatedAt: Date? = nil,
+    reviews: [Review] = []
+  ) {
+    self.id = id
+    self.firewood = firewood
+    self.createdAt = createdAt
+    self.updatedAt = updatedAt
+    self.reviews = reviews
+  }
+}
