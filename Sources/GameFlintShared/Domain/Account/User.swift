9c99ed985fc9e056561ee82ea6ef2b0e97406ea0
Reda Lemeden <git@redalemeden.com>
2020-11-29 13:00:34 +0100

    Update user struct

--- Sources/GameFlintShared/Domain/Account/User.swift
+++ Sources/GameFlintShared/Domain/Account/User.swift
@@ -1,17 +1,32 @@
 import Foundation
 
-public struct User: Codable, Equatable, Identifiable {
+public struct User: Codable, Equatable, Identifiable, Hashable {
   public var id: UUID
-  public var username: String
   public var isAdmin: Bool
-  
+  public var email: String
+  public var givenName: String?
+  public var familyName: String?
+  public var createdAt: Date?
+  public var updatedAt: Date?
+  public var profile: Profile?
+
   public init(
     id: UUID,
-    username: String,
-    isAdmin: Bool = false
+    isAdmin: Bool = false,
+    email: String,
+    givenName: String? = nil,
+    familyName: String? = nil,
+    profile: Profile? = nil,
+    createdAt: Date? = nil,
+    updatedAt: Date? = nil
   ) {
     self.id = id
-    self.username = username
     self.isAdmin = isAdmin
+    self.email = email
+    self.givenName = givenName
+    self.familyName = familyName
+    self.profile = profile
+    self.createdAt = createdAt
+    self.updatedAt = updatedAt
   }
 }
