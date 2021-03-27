314d3e5753ff434fd87aa4ef4ee3f3f939baa7af
Reda Lemeden <git@redalemeden.com>
2021-03-27 18:14:54 +0100

    Add public initializer

--- Sources/GameFlintShared/Domain/Game/Metadata/Metadata.swift
+++ Sources/GameFlintShared/Domain/Game/Metadata/Metadata.swift
@@ -4,4 +4,14 @@ public struct Metadata: Codable, Equatable, Hashable {
   public var igdb: IGDB
   public var metacritic: Metacritic
   public var openCritic: OpenCritic
+
+  public init(
+    igdb: Metadata.IGDB,
+    metacritic: Metadata.Metacritic,
+    openCritic: Metadata.OpenCritic
+  ) {
+    self.igdb = igdb
+    self.metacritic = metacritic
+    self.openCritic = openCritic
+  }
 }
