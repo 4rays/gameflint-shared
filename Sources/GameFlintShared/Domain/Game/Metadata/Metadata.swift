b597080c807233601750060215feb6b194a7f03a
Reda Lemeden <git@redalemeden.com>
2021-03-27 18:17:31 +0100

    Make metadata fields optional

--- Sources/GameFlintShared/Domain/Game/Metadata/Metadata.swift
+++ Sources/GameFlintShared/Domain/Game/Metadata/Metadata.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 public struct Metadata: Codable, Equatable, Hashable {
-  public var igdb: IGDB
-  public var metacritic: Metacritic
-  public var openCritic: OpenCritic
+  public var igdb: IGDB?
+  public var metacritic: Metacritic?
+  public var openCritic: OpenCritic?
 
   public init(
-    igdb: Metadata.IGDB,
-    metacritic: Metadata.Metacritic,
-    openCritic: Metadata.OpenCritic
+    igdb: Metadata.IGDB?,
+    metacritic: Metadata.Metacritic?,
+    openCritic: Metadata.OpenCritic?
   ) {
     self.igdb = igdb
     self.metacritic = metacritic
