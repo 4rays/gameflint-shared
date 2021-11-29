c715696b5038aacd18e17c030deacf0b3a7b1974
Reda Lemeden <git@redalemeden.com>
2021-11-29 12:30:56 +0100

    Fix tests

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
   name: "gameflint-shared",
   platforms: [
-    .macOS(.v10_15)
+    .iOS(.v15),
+    .macOS(.v12),
+    .tvOS(.v15),
+    .watchOS(.v8)
   ],
   products: [
     .library(
--- Tests/GameFlintSharedTests/PathComponentTests.swift
+++ Tests/GameFlintSharedTests/PathComponentTests.swift
@@ -3,39 +3,46 @@ import XCTest
 
 final class PathComponentTests: XCTestCase {
   func testPathComponents() {
-    let path1 = APIRoutes.v1.auth(.signIn)(.init())
-    let path2 = APIRoutes.v1.path(.init())
-    let path3 = APIRoutes.Auth.verifyPasswordToken.endpoint
+    XCTAssertEqual(
+      URLPath.signIn.apiVersion(.v1).fullPath,
+      "api/v1/auth/sign-in"
+    )
 
-    XCTAssertEqual(path1.fullPath, "api/v1/auth/sign-in")
-    XCTAssertEqual(path2.fullPath, "api/v1")
-    XCTAssertEqual(path3.fullPath, "auth/reset-password/verify")
+    XCTAssertEqual(
+      URLPath().apiVersion(.v1).fullPath,
+      "api/v1"
+    )
+
+    XCTAssertEqual(
+      URLPath.verifyPasswordToken.fullPath,
+      "auth/reset-password/verify"
+    )
   }
 
   func testImportGamesPath() {
-    let path = APIRoutes.v1.admin(.games(.import))(.init())
-    XCTAssertEqual(path.fullPath, "api/v1/admin/games/import")
+    XCTAssertEqual(
+      URLPath.importGames.apiVersion(.v1).fullPath,
+      "api/v1/admin/games/import"
+    )
   }
 
   func testUserGamesPath() {
-    let path = APIRoutes.v1.account(.userGames(.all))(.init())
-    XCTAssertEqual(path.fullPath, "api/v1/account/games")
+    XCTAssertEqual(
+      URLPath.userGames.apiVersion(.v1).fullPath,
+      "api/v1/account/games"
+    )
   }
 
   func testCustomOperator() {
-    let transform1 = "path".asPath
-    let transform2 = "to".asPath
-    let transform3 = "endpoint".asPath
-
-    let composed = transform1 / transform2 / transform3
-    let endpoint = composed(ResourcePath())
-
-    XCTAssertEqual(endpoint.fullPath, "path/to/endpoint")
+    let composed = "path" / "to" / "endpoint"
+    XCTAssertEqual(composed.fullPath, "path/to/endpoint")
   }
 
   func testFlareUpgamePath() {
     let id = UUID()
-    let endpoint = APIRoutes.v1.account(.games(.flareUp(id)))(.init())
-    XCTAssertEqual(endpoint.fullPath, "api/v1/games/\(id)/flare")
+    XCTAssertEqual(
+      URLPath.flareUp(with: id).apiVersion(.v1).fullPath,
+      "api/v1/games/\(id)/flare"
+    )
   }
 }
