bc6d39010c693981715bef19431c01bbe9729611
Reda Lemeden <git@redalemeden.com>
2022-12-24 12:52:22 +0100

    Nest ExternalGame in IGDB domain

--- Sources/GameFlintShared/Domain/Game/Metadata/ExternalGame.swift
+++ Sources/GameFlintShared/Domain/Game/Metadata/ExternalGame.swift
@@ -1,19 +1,32 @@
 import Foundation
 
-public struct ExternalGame: Codable, Hashable {
-  public var externalID: String
-  public var igdbID: Int
-  public var category: Int
-  public var url: URL?
+extension Metadata.IGDB {
+  public struct ExternalGame: Codable, Hashable {
+    public var externalID: String
+    public var igdbID: Int
+    public var category: Int
+    public var url: URL?
 
-  public var service: ExternalService? {
-    .init(category)
-  }
+    public var service: ExternalService? {
+      switch category {
+      case 1: return .steam
+      case 5: return .gog
+      case 11: return .xbox
+      case 13: return .appStore
+      case 14: return .twitch
+      case 15: return .playStore
+      case 26: return .epic
+      case 28: return .oculus
+      case 36: return .psn
+      default: return nil
+      }
+    }
 
-  public enum CodingKeys: String, CodingKey {
-    case externalID = "uid"
-    case igdbID = "game"
-    case category
-    case url
+    public enum CodingKeys: String, CodingKey {
+      case externalID = "uid"
+      case igdbID = "game"
+      case category
+      case url
+    }
   }
 }
--- Sources/GameFlintShared/Domain/Game/Metadata/ExternalService.swift
+++ Sources/GameFlintShared/Domain/Game/Metadata/ExternalService.swift
@@ -10,38 +10,4 @@ public enum ExternalService: Codable, Hashable {
   case oculus
   case psn
   case twitch
-
-  init?(_ value: Int) {
-    switch value {
-    case 1:
-      self = .steam
-
-    case 5:
-      self = .gog
-
-    case 11:
-      self = .xbox
-
-    case 13:
-      self = .appStore
-
-    case 14:
-      self = .twitch
-
-    case 15:
-      self = .playStore
-
-    case 26:
-      self = .epic
-
-    case 28:
-      self = .oculus
-
-    case 36:
-      self = .psn
-
-    default:
-      return nil
-    }
-  }
 }
