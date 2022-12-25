0aab7f3cb7ec27b91d7a8312bc427db17f8a8f18
Reda Lemeden <git@redalemeden.com>
2022-12-25 18:44:44 +0100

    Add external game metadata (#7)

---
+++ Sources/GameFlintShared/Domain/Game/Metadata/ExternalService.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public enum ExternalService: Codable, Hashable {
+  case appStore
+  case epic
+  case gog
+  case oculus
+  case playStore
+  case psn
+  case steam
+  case twitch
+  case xbox
+}
--- Sources/GameFlintShared/Domain/Game/Metadata/Metadata.swift
+++ Sources/GameFlintShared/Domain/Game/Metadata/Metadata.swift
@@ -4,14 +4,41 @@ public struct Metadata: Codable, Hashable {
   public var igdb: IGDB?
   public var metacritic: Metacritic?
   public var openCritic: OpenCritic?
+  public var playStoreID: String? // "com.mojang.minecraftpe"
+  public var appStoreID: String? // "479516143"
+  public var epicID: String? // "b4daa3e4-a782-401f-a68e-52f6b2d0a1ad"
+  public var gogID: String? // "1443699418"
+  public var oculusID: String? // "2557519677620753"
+  public var psnID: String? // "212779"
+  public var steamID: String? // "72850"
+  public var twitchID: String? // "27471"
+  public var xboxID: String? // "9NBLGGH537BL"
 
   public init(
     igdb: Metadata.IGDB? = nil,
     metacritic: Metadata.Metacritic? = nil,
-    openCritic: Metadata.OpenCritic? = nil
+    openCritic: Metadata.OpenCritic? = nil,
+    playStoreID: String? = nil,
+    appStoreID: String? = nil,
+    epicID: String? = nil,
+    gogID: String? = nil,
+    oculusID: String? = nil,
+    psnID: String? = nil,
+    steamID: String? = nil,
+    twitchID: String? = nil,
+    xboxID: String? = nil
   ) {
     self.igdb = igdb
     self.metacritic = metacritic
     self.openCritic = openCritic
+    self.playStoreID = playStoreID
+    self.appStoreID = appStoreID
+    self.epicID = epicID
+    self.gogID = gogID
+    self.oculusID = oculusID
+    self.psnID = psnID
+    self.steamID = steamID
+    self.twitchID = twitchID
+    self.xboxID = xboxID
   }
 }
