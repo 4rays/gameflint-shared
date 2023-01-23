a069b727cf0e1a945c3988f46d579003717a30f1
Reda Lemeden <git@redalemeden.com>
2023-01-23 02:13:39 +0100

    Refactor release date format (#10)

--- Sources/GameflintShared/Domain/Game/Release/Release.swift
+++ Sources/GameflintShared/Domain/Game/Release/Release.swift
@@ -2,50 +2,101 @@ import Foundation
 
 public struct Release: Codable, Hashable, Comparable, Identifiable, Sendable {
   public var id: UUID
-  public var isTentative: Bool
-  public private(set) var date: Date?
-  public var timeZone: String?
+  public var date: Date?
   public var descriptions: [Language: String]
   public var platforms: [Platform]
   public var regions: [Region]
-  public var createdAt: Date?
-  public var updatedAt: Date?
+  public var createdAt: Foundation.Date?
+  public var updatedAt: Foundation.Date?
 
-  public var humanDate: ReleaseDate? {
-    didSet {
-      date = try? humanDate?.toRawDate()
-    }
+  public enum Date: Codable, Hashable, Sendable, Comparable {
+    case official(Foundation.Date)
+    case tentative(ReleaseDate)
   }
 
   public init(
     id: UUID,
-    isTentative: Bool = false,
     platforms: [Platform] = [],
     regions: [Region] = [],
-    humanDate: ReleaseDate? = nil,
-    timeZone: String? = nil,
+    date: Date? = nil,
     descriptions: [Language: String] = [:],
-    createdAt: Date? = nil,
-    updatedAt: Date? = nil
+    createdAt: Foundation.Date? = nil,
+    updatedAt: Foundation.Date? = nil
   ) {
     self.id = id
-    self.isTentative = isTentative
-    self.humanDate = humanDate
-    self.date = try? humanDate?.toRawDate()
-    self.timeZone = timeZone
+    self.date = date
     self.platforms = platforms
     self.regions = regions
     self.createdAt = createdAt
     self.updatedAt = updatedAt
     self.descriptions = descriptions
   }
+
+  public init(
+    id: UUID,
+    platforms: [Platform] = [],
+    regions: [Region] = [],
+    isTentative: Bool,
+    date: ReleaseDate? = nil,
+    descriptions: [Language: String] = [:],
+    createdAt: Foundation.Date? = nil,
+    updatedAt: Foundation.Date? = nil
+  ) {
+    self.id = id
+    self.platforms = platforms
+    self.regions = regions
+    self.createdAt = createdAt
+    self.updatedAt = updatedAt
+    self.descriptions = descriptions
+
+    if isTentative,
+      let tentativeDate = date {
+      self.date = .tentative(tentativeDate)
+    } else if let officialDate = try? date?.toUTCDate() {
+      self.date = .official(officialDate)
+    }
+  }
+
+  public var officialDate: Foundation.Date? {
+    switch date {
+    case .official(let date):
+      return date
+    default:
+      return nil
+    }
+  }
+}
+
+extension Release.Date {
+  public static func < (lhs: Release.Date, rhs: Release.Date) -> Bool {
+    switch (lhs, rhs) {
+    case let (.official(lhsDate), .official(rhsDate)):
+      return lhsDate < rhsDate
+
+    case let (.tentative(lhsDate), .tentative(rhsDate)):
+      return lhsDate < rhsDate
+    
+    case (.tentative, .official):
+      return true
+
+    case (.official, .tentative):
+      return false
+
+    default:
+      return false
+    }
+  }
 }
 
 extension Release {
   public static func < (lhs: Release, rhs: Release) -> Bool {
-    switch (lhs.humanDate, rhs.humanDate) {
+    switch (lhs.date, rhs.date) {
     case let (.some(lhd), .some(rhd)):
       return lhd < rhd
+
+    case (.none, .some):
+      return true
+
     default:
       return false
     }
