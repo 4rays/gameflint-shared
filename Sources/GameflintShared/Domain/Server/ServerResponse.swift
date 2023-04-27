2a451931e88cb6756e5d429a380110c8258fb716
Reda Lemeden <git@redalemeden.com>
2023-04-27 02:29:46 +0100

    Make ServerResponse an enum

--- Sources/GameflintShared/Domain/Server/ServerResponse.swift
+++ Sources/GameflintShared/Domain/Server/ServerResponse.swift
@@ -1,11 +1,33 @@
 import Foundation
 
-public struct ServerResponse: Codable, Equatable, Sendable {
-  public var error: CodedError?
-  public var success: Bool
+public enum ServerResponse: Equatable, Sendable {
+  case failure(CodedError)
+  case success
+}
 
-  public init(error: CodedError? = nil) {
-    self.error = error
-    self.success = error == nil
+extension ServerResponse: Codable {
+  enum CodingKeys: String, CodingKey {
+    case failure
+    case success
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    if let failure = try container.decodeIfPresent(CodedError.self, forKey: .failure) {
+      self = .failure(failure)
+    } else {
+      self = .success
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    switch self {
+    case let .failure(error):
+      try container.encode(error, forKey: .failure)
+
+    case .success:
+      try container.encode(true, forKey: .success)
+    }
   }
 }
