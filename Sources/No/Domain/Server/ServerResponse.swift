81a0422bbca8c21ff274d649ad0df76bea618766
Reda Lemeden <git@redalemeden.com>
2023-10-27 18:36:00 +0200

    Rename path

--- Sources/No/Domain/Server/ServerResponse.swift
+++ Sources/No/Domain/Server/ServerResponse.swift
@@ -7,13 +7,14 @@ public enum ServerResponse: Equatable, Sendable {
 
 extension ServerResponse: Codable {
   enum CodingKeys: String, CodingKey {
-    case failure
+    case error
     case success
   }
 
   public init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
-    if let failure = try container.decodeIfPresent(CodedError.self, forKey: .failure) {
+
+    if let failure = try container.decodeIfPresent(CodedError.self, forKey: .error) {
       self = .failure(failure)
     } else {
       self = .success
@@ -24,7 +25,7 @@ extension ServerResponse: Codable {
     var container = encoder.container(keyedBy: CodingKeys.self)
     switch self {
     case let .failure(error):
-      try container.encode(error, forKey: .failure)
+      try container.encode(error, forKey: .error)
 
     case .success:
       try container.encode(true, forKey: .success)
